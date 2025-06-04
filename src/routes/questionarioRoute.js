fcd90c5cf5596e936736fb0c7b10692b3dc277d1
FelipeLima24 <felipe.qlima@sptech.school>
2025-06-04 11:02:07 -0300

    atualizacaoFinalHTML

--- src/routes/questionarioRoute.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router  = express.Router();
-var questionarioController = require("../controllers/questionarioController");
-
-router.post("/criar", questionarioController.criar);
-
-
-
-module.exports = router;
